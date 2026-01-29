0b4989d55893633c9aabd221247c16e9ec3fa1e6
Harshit Garg <gargh2120@gmail.com>
2026-01-30 00:03:15 +0530

    This compoenents describes Complexity with props .!

--- src/Components/ComplexProps.tsx
+++ src/Components/ComplexProps.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function ComplexProps() {
   return (
-    <div>ComplexProps</div>
-  )
+    <div className={` bg-white p-8 rounded-2xl mt-8 m-4`}>
+      <div className={`mb-2`}>
+        <h1 className={`text-4xl text-black mb-3 font-bold`}>
+          Complex/Nested Props
+        </h1>
+        <p className={`text-gray-500 font-semibold text-xl`}>
+          Complex props allows you to pass nested objects and functions,
+          enabling sophisticated component configuration and interactions.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default ComplexProps
+export default ComplexProps;
