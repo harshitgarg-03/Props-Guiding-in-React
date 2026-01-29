72abcd4fb50cf392aa46bc2ba87fa275144c358f
Harshit Garg <gargh2120@gmail.com>
2026-01-30 02:03:21 +0530

    This compoenents describes Complexity with props .!

--- src/Components/ComplexProps.tsx
+++ src/Components/ComplexProps.tsx
@@ -1,4 +1,110 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+interface ProfileCardProps {
+  user : {
+        name: string,
+        email: string,
+        avatar: string,
+        role: string,
+        status: string,
+        stats?: {
+          posts?: number,
+          followers?: number,
+          following?: number,
+          Project?: number,
+          Commits?: number,
+          Reviews?: number
+        }
+  }
+
+  theme : {
+        backgroundColor: string,
+        textColor: string,
+        avatarBg: string,
+        badgeBg: string
+  }
+
+  actions : {
+        primary: {
+          label: string,
+          onClick: () => string,
+          className: string,
+        },
+        secondary: {
+          label: string,
+          onClick: () => string,
+          className: string,
+        },
+  }
+}
+function ProfileCard({user, theme, actions} : ProfileCardProps){
+  return (
+    <div className={`${theme.backgroundColor} ${theme.textColor} p-8 m-4 mt-8 rounded-2xl`} >
+
+
+     
+      <div className="flex items-center gap-6 mb-4">
+       
+        <div
+          className={`${theme.avatarBg} text-3xl w-14 h-14 flex items-center justify-center rounded-full`}
+        >
+          {user.avatar}
+        </div>
+
+        
+        <div>
+          <h3 className="text-xl font-semibold">{user.name}</h3>
+          <p className="text-sm mb-2 opacity-80">{user.email}</p>
+
+         
+          <div className="flex gap-2 mt-1">
+            <span
+              className={`${theme.badgeBg} px-3 py-1 rounded-full text-xs font-medium`}
+            >
+              {user.role}
+            </span>
+
+            <span
+              className={`${theme.badgeBg} px-3 py-1 rounded-full text-xs font-medium`}
+            >
+              {user.status}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <hr className="my-4 opacity-30" />
+
+      {user.stats && (
+        <div className="grid grid-cols-3 text-center mb-5">
+          {Object.entries(user.stats).map(([key, value]) => (
+            <div key={key}>
+              <div className="text-2xl font-bold">{value}</div>
+              <div className="text-sm capitalize opacity-70">{key}</div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="flex gap-3">
+        <button
+          onClick={actions.primary.onClick}
+          className={`flex-1 py-2 rounded-xl cursor-pointer font-semibold ${actions.primary.className}`}
+        >
+          {actions.primary.label}
+        </button>
+
+        <button
+          onClick={actions.secondary.onClick}
+          className={`flex-1 py-2 rounded-xl  cursor-pointer font-semibold ${actions.secondary.className}`}
+        >
+          {actions.secondary.label}
+        </button>
+      </div>
+      
+    </div>
+  )
+}
 
 function ComplexProps() {
   const [Message, setMessage] = useState<string>("User's Profile")
@@ -18,10 +124,10 @@ function ComplexProps() {
       },
   
       theme: {
-        backgroundColor: "bg-gradient-to-br from-purple-100 to-blue-100",
+        backgroundColor: "bg-gradient-to-br from-purple-300 to-blue-100",
         textColor: "text-gray-800",
-        avatarBg: "bg-purple-300",
-        badgeBg: "bg-purple-200",
+        avatarBg: "bg-purple-500",
+        badgeBg: "bg-purple-400",
       },
   
       actions: {
@@ -52,10 +158,10 @@ function ComplexProps() {
       },
   
       theme: {
-        backgroundColor: "bg-gradient-to-br from-green-100 to-blue-100",
+        backgroundColor: "bg-gradient-to-br from-green-300 to-blue-100",
         textColor: "text-gray-800",
-        avatarBg: "bg-green-300",
-        badgeBg: "bg-green-200",
+        avatarBg: "bg-green-400",
+        badgeBg: "bg-green-300",
       },
   
       actions: {
@@ -85,9 +191,15 @@ function ComplexProps() {
       </div>
 
       <div
-        className={`px-8 py-4 bg-sky-200 border-l-4 border-sky-500 rounded-2xl font-sans font-semibold font-light text-xl text-sky-700`}
+        className={`px-8 py-4 bg-sky-200 border-l-4 border-sky-500 rounded-2xl font-sans font-semibold  text-xl text-sky-700`}
       >
         <h2>{Message}</h2>
+      </div>
+
+      <div className={`grid gap-6 grid-cols-1 md:grid-cols-2`} >
+        {Users.map((userData, idx) => (
+          <ProfileCard key={idx} {...userData}/>
+        ))}
       </div>
     </div>
   );
