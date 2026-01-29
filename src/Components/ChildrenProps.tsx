635438d86a8311ce16e485e8b0b2e41f0d140640
Harshit Garg <gargh2120@gmail.com>
2026-01-29 22:32:47 +0530

    This compoenents describes Children props .!

--- src/Components/ChildrenProps.tsx
+++ src/Components/ChildrenProps.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 
+
+interface Props {
+  layout?: string;
+  children? : any;
+  "vertical"? : string;
+  "horizontal"? : string;
+  "grid"? : string;
+
+}
+
+function Card () {
+
+}
+
+function Container ({children, layout = "vertical"} : Props){
+  const LayoutClasses : Props = {
+    "vertical" : "flex flex-col space-y-4",
+    "grid" : "grid grid-cols-1 md:grid-cols-2 gap-4",
+    "horizontal" : "flex flex-row flex-wrap gap-4"
+  }
+
+
+}
 function ChildrenProps() {
   return (
     <div className={` bg-white p-8 rounded-2xl mt-8 m-4`}>
-      <div className={`mb-2`}>
+      <div className={`mb-6`}>
         <h1 className={`text-4xl text-black mb-3 font-bold`}>Children Props</h1>
         <p className={`text-gray-500 font-semibold text-xl`}>
-          The children prop allows you to pass TSX elements or components as
+          The <span className="text-gray-500 font-italic bg-gray-200 px-1 p-1 rounded-xl">children</span> children prop allows you to pass TSX elements or components as
           childrens to other components, enabling component composition.
         </p>
       </div>
+
+      <h3 className={`font-sans font-bold text-2xl mb-3 text-gray-600`}>Card Componets with Children:</h3>
+
     </div>
   );
 }
