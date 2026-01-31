5b0b50e0022e080861766109d313df16338e3195
Harshit Garg <gargh2120@gmail.com>
2026-01-31 23:42:12 +0530

    Hook for using usetheme !

--- src/hook/useTheme.tsx
+++ src/hook/useTheme.tsx
@@ -1,11 +1,11 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../ContextApi/ThemeContext';
+import { useContext } from "react";
+import { ThemeContext } from "../ContextApi/ThemeContext";
 
 function useTheme() {
-    const context  = useContext(ThemeContext);
-    if(!context) {
-        throw new Error("Error in Contexting..")
-    }
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("Error in Contexting..");
+  }
   return context;
 }
 
