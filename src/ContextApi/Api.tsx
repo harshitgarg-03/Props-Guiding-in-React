ae6386c452eeb22399602d1bf6f2fd2179587811
Harshit Garg <gargh2120@gmail.com>
2026-01-31 23:42:52 +0530

    using toogle theme through context api !

--- src/ContextApi/Api.tsx
+++ src/ContextApi/Api.tsx
@@ -2,26 +2,25 @@ import { useState, type ReactNode } from "react";
 export interface valueProps {
   Theme: string;
   isDark: boolean;
-  ToogleTheme: () => void
+  ToogleTheme: () => void;
 }
 
 import { ThemeContext } from "./ThemeContext";
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-    console.log("Hello");
-    
+  console.log("Hello");
+
   const [Theme, setTheme] = useState("light");
 
-
   const ToogleTheme = () => {
-    setTheme((prevT) => (prevT==="light" ? "dark": "light"));
-    
+    setTheme((prevT) => (prevT === "light" ? "dark" : "light"));
   };
 
   const value: valueProps = {
     Theme,
     ToogleTheme,
-    isDark: Theme === "dark"
+    isDark: Theme === "dark",
   };
-  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 };
-
